63f4964dbafb3cfe6f8900a21770c852d56a8ecd
Thuy-Vy Nguyen <thuyvynguy@gmail.com>
2021-03-08 15:03:21 -0800

    Update CocktailSearchViewModel.java

--- app/src/main/java/com/cs492/cocktailapp/api/CocktailSearchViewModel.java
+++ app/src/main/java/com/cs492/cocktailapp/api/CocktailSearchViewModel.java
@@ -1,9 +1,10 @@
-package com.cs492.cocktailapp.data;
+package com.cs492.cocktailapp.api;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.ViewModel;
 
 import com.cs492.cocktailapp.api.CocktailSearchRepository;
+import com.cs492.cocktailapp.data.CocktailSearchItem;
 
 import java.util.List;
 
