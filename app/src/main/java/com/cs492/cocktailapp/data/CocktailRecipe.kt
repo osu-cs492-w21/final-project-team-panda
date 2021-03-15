0134b09c120489812653fda0b52705a20c87c379
Julian Fortune <fortunej@oregonstate.edu>
2021-03-14 19:30:07 -0700

    Tweaks to the detail activity

--- app/src/main/java/com/cs492/cocktailapp/data/CocktailRecipe.kt
+++ app/src/main/java/com/cs492/cocktailapp/data/CocktailRecipe.kt
@@ -11,3 +11,20 @@ data class CocktailRecipe(
         val drinkGlass: String,
         val ingredientList: ArrayList<MeasureIngredient>
 ) : Serializable {
+
+    val ingredientSummary: String
+        get() = {
+            when (ingredientList.size) {
+                0 -> ""
+                1 -> ingredientList[0].ingredient + "."
+                2 -> "${ingredientList[0].ingredient} and ${ingredientList[1].ingredient}."
+                else -> {
+                    val end = ingredientList.size - 1
+                    ingredientList.slice(0 until end).joinToString {
+                        it.ingredient
+                    } + ", and ${ingredientList[end].ingredient}."
+                }
+            }
+        }()
+
+}
